0569039e844710af3861688a2ee4df25b6ccc4cf
Yoana Chung <yoana@yoanachung.com>
2022-08-25 15:10:29 +0900

    add polls' detail view

--- polls/views.py
+++ polls/views.py
@@ -6,9 +6,11 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-    
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
