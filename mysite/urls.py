0d39d9053501ce750df5d81bc0ec308833931d4b
Yoana Chung <yoana@yoanachung.com>
2022-08-27 00:12:58 +0900

    djangorestframework settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
