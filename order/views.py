da58b1906d5acdb95ff29be76d415b1b2ff98dac
Yoana Chung <yoana@yoanachung.com>
2022-08-29 15:36:05 +0900

    add menu apis

--- order/views.py
+++ order/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from order.models import Shop, Menu, Order, OrderFood
-from order.serializers import ShopSerializer
+from order.serializers import ShopSerializer, MenuSerializer
 
 @csrf_exempt
 def shop(request):
@@ -14,6 +14,20 @@ def shop(request):
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def menu(request, shop):
+    if request.method == 'GET':
+        menu = Menu.objects.filter(shop=shop)
+        serializer = MenuSerializer(menu, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = MenuSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
