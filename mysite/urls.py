09c4b6c183df09c0c4705d615e8dbadc79aa383f
Yoana Chung <yoana@yoanachung.com>
2022-08-29 20:22:24 +0900

    create owner app

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('order/', include('order.urls')),
+    path('owner/', include('owner.urls')),
 ]
---
+++ owner/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from owner import views
+
+app_name = 'owner'
+urlpatterns = [
+]
